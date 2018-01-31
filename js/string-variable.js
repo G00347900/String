fd3d5cdd63df7181c6a6b2ba82ab22828311c874
G00347900 <35769360+G00347900@users.noreply.github.com>
2018-01-31 13:39:25 +0000

    Add files via upload

---
+++ js/string-variable.js
@@ -0,0 +1,17 @@
+// create variables to hold the name and note text.
+var username;
+var message;
+
+// Assign values to these variables.
+username = "Molly";
+message = "See our upcoming range";
+
+// Get the element with and id of name.
+var alName = document.getElementyById("name");
+// Replace the content of this element.
+elName.textContent = username;
+
+//Get the element with an id of note.
+var elNote = document.getElementById("Note");
+// Replace the content of this element.
+elNote.textContent = message; 
